553772f81f681abbbb53cacc242f4aae4cd6d36b
augusto neto <augustoneto0213@gmail.com>
2025-12-10 20:16:47 -0300

    export default/ REACT

---
+++ src/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+ function Footer(){
+    return(
+        <>FOOTER</>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Header from "../layouts/Header.tsx"
+import Footer from "../layouts/Footer.tsx";
+
+function Home(){
+    return(
+        <>
+            <Header />
+            
+            <br /><br /><br /><br />
+
+            < Footer />
+        </> 
+    );
+}
+
+export default Home ;
